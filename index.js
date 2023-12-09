98724ffe17317f0c294534e4dea7e4b138f0ff36
Fraim <fraim263@gmail.com>
2023-12-09 15:05:26 +0300

    adding search

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const removeLoadScreen = () => {
     document.querySelector('.load-screen').remove();
     document.querySelector('.loaded').style.visibility = 'visible';
   }, 1000);
+};
+
+const renderSearchResult = (data) => {
+  const $ul = document.querySelector('.mdl-list');
+
+  if (!data.length) return $ul.innerHTML = '';
+
+  $ul.innerHTML = data.map((address) => {
+    return `
+      <li class="mdl-list__item">
+        <div class="mdl-list__item-primary-content">
+          ${address.name}
+        </div>
+        <div>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+            <span
+                data-pos="[${address.center.lat}, ${address.center.lng}]"
+                class="material-symbols-outlined">location_on</span>
+          </button>
+        </div>
+        <div>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
+            <span class="material-symbols-outlined">add</span>
+          </button>
+        </div>
+      </li>
+    `
+  }).join('');
 }
+
+const requestGeocodeByQuery = throttle((geocoder, query) => {
+  if (!query) return renderSearchResult([]);
+
+  geocoder.suggest(query, (data) => {
+    renderSearchResult(data)
+  })
+}, 1000);
 
 const initMap = () => {
   const mapContainer = document.querySelector('.map');
@@ -34,14 +70,24 @@ const initMap = () => {
 
   layers.$2gis.addTo(map);
 
-  return { map, layers, geolocationLayers };
+  const geocoder = L.Control.Geocoder.photon({
+    defaultMarkGeocode: false,
+    showUniqueResult: true,
+  });
+
+  return { map, layers, geolocationLayers, geocoder };
 };
 
 const getMeMarker = (pos) => L.circleMarker(pos, { radius: 25, color: '#536dfe', stroke: false, fillOpacity: 0.3 });
 const getMeRadiusMarker = (pos) => L.circleMarker(pos, { radius: 10, color: '#ff1744', stroke: false, fillOpacity: 1 });
 
 window.addEventListener('load',() => {
-  const { map, layers, geolocationLayers } = initMap();
+  const {
+    map,
+    layers,
+    geolocationLayers,
+    geocoder,
+  } = initMap();
 
   navigator.geolocation.getCurrentPosition(
     ({ coords }) => {
@@ -68,6 +114,13 @@ window.addEventListener('load',() => {
 
   const menu = new MaterialMenu(document.querySelector('.mdl-menu'));
   const $tilesButton = document.querySelector('.tiles');
+  const $searchButton = document.querySelector('.search');
+  const $searchDialog = document.querySelector('.search-content');
+  const $searchInput = document.querySelector('#search-input');
+
+  if (!$searchDialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
+  }
 
   menu.element_.addEventListener('click', e => {
     if (e.target.hasAttribute('data-layer')) {
@@ -84,8 +137,51 @@ window.addEventListener('load',() => {
     }
   });
 
-  $tilesButton.addEventListener('click', (e) => {
-      alert(JSON.stringify(e.target))
-      menu.toggle();
+  $tilesButton.addEventListener('click', (e) => menu.toggle());
+
+  $searchButton.addEventListener('click', () => {
+    $searchDialog.showModal()
   });
+
+  $searchDialog.addEventListener('click', function ({ target, stopPropagation }) {
+    if (target.nodeName === 'DIALOG') {
+      target.close()
+    } else if (target.nodeName === 'SPAN') {
+      const pos = JSON.parse(target.getAttribute('data-pos'));
+      map.setView(pos, 20, { animate: true });
+      $searchDialog.close();
+    }
+  });
+
+  $searchInput.addEventListener('input', ({ target }) => {
+    requestGeocodeByQuery(geocoder, target.value);
+  })
 })
+
+function throttle(func, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function() {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
